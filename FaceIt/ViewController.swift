6002c9bc665b586d07703a36daf20a7b53215310
Matt Wang <mattwyl@gmail.com>
2017-07-17 21:51:37 +0200

    Initial skeleton

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Matt Wang on 7/17/17.
+//  Copyright © 2017 Joyus Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
